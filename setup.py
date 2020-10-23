2d2ef6e50ba7e0eee862537394dd144c8d91d731
Ayush Modi <hot9cups@yahoo.in>
2020-10-23 18:47:11 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name = 'Prob-Dists-GB',
+      version = '1.0',
+      description = 'Python Package for modelling Gaussian and Binomial distributions',
+      packages = ['Prob-Dists-GB'],
+      author = 'Ayush Modi',
+      author_email = 'hot9cups@yahoo.in',
+      zip_safe=False)
