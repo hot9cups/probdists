6db169da1d3aaa563575a47a10b367ce34d8f659
Ayush Modi <hot9cups@yahoo.in>
2020-10-26 21:24:20 +0530

    Update test.py
    fixed flake8 and broken code

--- test.py
+++ test.py
@@ -2,45 +2,54 @@ import unittest
 
 from probdists import Gaussian
 from probdists import Binomial
-from probdists import Exponential 
+from probdists import Exponential
+
 
 class TestGaussianClass(unittest.TestCase):
     def setUp(self):
         self.gaussian = Gaussian(25, 2)
         self.gaussian.read_data_file('numbers.txt')
 
-    def test_initialization(self): 
+    def test_initialization(self):
         self.assertEqual(self.gaussian.mean, 25, 'incorrect mean')
-        self.assertEqual(self.gaussian.stdev, 2, 'incorrect standard deviation')
+        self.assertEqual(self.gaussian.stdev, 2,
+                         'incorrect standard deviation')
 
     def test_readdata(self):
-        self.assertEqual(self.gaussian.data,\
-         [1, 3, 99, 100, 120, 32, 330, 23, 76, 44, 31], 'data not read in correctly')
+        self.assertEqual(self.gaussian.data,
+                         [1, 3, 99, 100, 120, 32, 330, 23, 76, 44, 31],
+                         'data not read in correctly')
 
     def test_meancalculation(self):
-        self.assertEqual(self.gaussian.calculate_mean(),\
-         sum(self.gaussian.data) / float(len(self.gaussian.data)), 'calculated mean not as expected')
+        self.assertEqual(self.gaussian.calculate_mean(),
+                         sum(self.gaussian.data) /
+                         float(len(self.gaussian.data)),
+                         'calculated mean not as expected')
 
     def test_stdevcalculation(self):
-        self.assertEqual(round(self.gaussian.calculate_stdev(), 2), 92.87, 'sample standard deviation incorrect')
-        self.assertEqual(round(self.gaussian.calculate_stdev(0), 2), 88.55, 'population standard deviation incorrect')
+        self.assertEqual(round(self.gaussian.calculate_stdev(), 2),
+                         92.87, 'sample standard deviation incorrect')
+        self.assertEqual(round(self.gaussian.calculate_stdev(
+            0), 2), 88.55, 'population standard deviation incorrect')
 
     def test_pdf(self):
-        self.assertEqual(round(self.gaussian.pdf(25), 5), 0.19947,\
-         'pdf function does not give expected result') 
+        self.assertEqual(round(self.gaussian.pdf(25), 5), 0.19947,
+                         'pdf function does not give expected result')
         self.gaussian.calculate_mean()
         self.gaussian.calculate_stdev()
-        self.assertEqual(round(self.gaussian.pdf(75), 5), 0.00429,\
-        'pdf function after calculating mean and stdev does not give expected result')      
+        self.assertEqual(round(self.gaussian.pdf(75), 5), 0.00429,
+                         'pdf function after calculating mean and \
+                             stdev does not give expected result')
 
     def test_add(self):
         gaussian_one = Gaussian(25, 3)
         gaussian_two = Gaussian(30, 4)
         gaussian_sum = gaussian_one + gaussian_two
-        
+
         self.assertEqual(gaussian_sum.mean, 55)
         self.assertEqual(gaussian_sum.stdev, 5)
-        
+
+
 class TestBinomialClass(unittest.TestCase):
     def setUp(self):
         self.binomial = Binomial(0.4, 20)
@@ -51,26 +60,27 @@ class TestBinomialClass(unittest.TestCase):
         self.assertEqual(self.binomial.n, 20, 'n value incorrect')
 
     def test_readdata(self):
-        self.assertEqual(self.binomial.data,\
-         [0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0], 'data not read in correctly')
-    
+        self.assertEqual(self.binomial.data,
+                         [0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0],
+                         'data not read in correctly')
+
     def test_calculatemean(self):
         mean = self.binomial.calculate_mean()
         self.assertEqual(mean, 8)
-    
+
     def test_calculatestdev(self):
         stdev = self.binomial.calculate_stdev()
-        self.assertEqual(round(stdev,2), 2.19)
-        
+        self.assertEqual(round(stdev, 2), 2.19)
+
     def test_replace_stats_with_data(self):
         p, n = self.binomial.replace_stats_with_data()
-        self.assertEqual(round(p,3), .615)
+        self.assertEqual(round(p, 3), .615)
         self.assertEqual(n, 13)
-        
+
     def test_pdf(self):
         self.assertEqual(round(self.binomial.pdf(5), 5), 0.07465)
         self.assertEqual(round(self.binomial.pdf(3), 5), 0.01235)
-    
+
         self.binomial.replace_stats_with_data()
         self.assertEqual(round(self.binomial.pdf(5), 5), 0.05439)
         self.assertEqual(round(self.binomial.pdf(3), 5), 0.00472)
@@ -79,38 +89,43 @@ class TestBinomialClass(unittest.TestCase):
         binomial_one = Binomial(.4, 20)
         binomial_two = Binomial(.4, 60)
         binomial_sum = binomial_one + binomial_two
-        
+
         self.assertEqual(binomial_sum.p, .4)
         self.assertEqual(binomial_sum.n, 80)
-        
+
+
 class TestExponentialClass(unittest.TestCase):
     def setup(self):
         self.exponential = Exponential(0.25)
         self.exponential.read_data_file('numbers_exponential.txt')
 
     def test_initialization(self):
-        self.assertEqual(self.exponential.mean, 4.0 , 'incorrect mean')
-        self.assertEqual(self.exponential.stdev, 4.0 , 'incoorect standard deviation')
+        self.assertEqual(self.exponential.mean, 4.0, 'incorrect mean')
+        self.assertEqual(self.exponential.stdev, 4.0,
+                         'incoorect standard deviation')
 
     def test_readdata(self):
-        self.assertEqual(self.exponential.data, \
-                [1, 3, 99, 100, 120, 32, 330, 23, 76, 44, 31], 'data read incorrectly')
-    
+        self.assertEqual(self.exponential.data,
+                         [1, 3, 99, 100, 120, 32, 330, 23, 76, 44, 31],
+                         'data read incorrectly')
+
     def test_meancalculation(self):
-        self.assertEqual(self.exponential.calculate_mean(), \
-                (1.0/0.25) , 'calculated mean not as expected')
+        self.assertEqual(self.exponential.calculate_mean(),
+                         (1.0 / 0.25),
+                         'calculated mean not as expected')
 
     def test_stdevcalculation(self):
-        self.assertEqual(self.exponential.calculate_stdev(), \
-                (1.0/0.25), 'calculated standard deviation incorrect')
+        self.assertEqual(self.exponential.calculate_stdev(),
+                         (1.0 / 0.25),
+                         'calculated standard deviation incorrect')
 
     def test_pdf(self):
-        self.assertEqual(round(self.exponential.pdf(1), 5, 0.19470, \
-                'pdf function does not give expexted result')
-        self.exponential.calculate_mean()
-        self.exponential.calculate_stdev()
-        self.assertEqual(round(self.exponential.pdf(5), 5), 0.07162, \
-                'pdf function after calculating mean and stdev does not give expected result')
+        self.assertEqual(round(self.exponential.pdf(1), 5), 0.19470,
+                         'pdf function does not give expexted result'),
+        self.assertEqual(round(self.exponential.pdf(5), 5), 0.07162,
+                         'pdf function after calculating mean and \
+                              stdev does not give expected result')
+
 
 if __name__ == '__main__':
     unittest.main()
