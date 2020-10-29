d9e2204ce72ebd8c99bf0067e59ab1ea9bd93ba2
Basil Labib <basillabib01@gmail.com>
2020-10-29 09:08:41 +0530

    Add unittest and usage for Bernoulli Distribution

--- probdists/Generaldistribution.py
+++ probdists/Generaldistribution.py
@@ -41,9 +41,14 @@ class Distribution:
         elif file_name == 'demo_exponential_data':
             dirname = Path(__file__).parent.parent.absolute()
             file_name = Path(dirname, 'numbers_exponential.txt')
+
         elif file_name == 'demo_gamma_data':
             dirname = Path(__file__).parent.parent.absolute()
             file_name = Path(dirname, 'numbers_gamma.txt')
+
+        elif file_name == 'demo_bernoulli_data':
+            dirname = Path(__file__).parent.parent.absolute()
+            file_name = Path(dirname, 'numbers_exponential.txt')
 
         with open(file_name) as file:
             data_list = []
