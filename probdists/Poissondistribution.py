ff16c14755e18faba8eebcc24cc6af8ad34b0a0d
Saima <saimalabib16@gmail.com>
2021-10-11 10:41:03 +0530

    Add pydocs: Poisson Distribution

--- probdists/Poissondistribution.py
+++ probdists/Poissondistribution.py
@@ -3,7 +3,18 @@ import matplotlib.pyplot as plt
 from .Generaldistribution import Distribution
 
 class Poisson(Distribution):
-    """
+    """ Poisson distribution class for calculating and 
+    visualizing a Poisson distribution.
+
+    Attributes:
+
+        mean (float): the mean value of the distribution
+        stdev (float): the standard deviation of the distribution
+
+        data (list of floats): extracted from the data file
+
+        lmbda (float): rate of the poisson distribution (missing an 'a' to prevent name clash with Python keyword)
+
     """
 
     def __init__(self, lmbda):
@@ -14,29 +25,53 @@ class Poisson(Distribution):
                             self.calculate_stdev())
 
     def calculate_mean(self, round_to=2):
-        """
+        """ Method to calculate the mean from lambda
+
+        Args:
+            round_to (int): Round the mean value. [Default value: 2 floating point]
+
+        Returns:
+            float: mean of the distribution
         """
         self.mean = math.sqrt(self.lmbda)
 
         return round(self.mean, round_to)
     
     def calculate_stdev(self, round_to=2):
-        """
+        """ Method to calculate the standard deviation from lmbda
+
+        Args:
+            round_to (int): Round the mean value. [Default value: 2 floating point]
+
+        Returns:
+            float: standard deviation of the distribution
         """
         self.stdev = math.sqrt(self.lmbda)
 
         return round(self.stdev, round_to)
     
     def calculate_pdf(self, x, round_to=2):
-        """
+        """ Probability density function calculator for the Poisson distribution.
+
+        Args:
+            x (float): point for caluclating the probability density function
+            round_to (int): Round the mean value. [Default value: 2 floating point]
+
+        Returns:
+            float: probability density function
         """
 
-        fact = math.factorial(x)
-        self.pdf = ( math.exp(-self.lmbda) * self.lmbda ** x) / fact
+        self.pdf = _calc_discrete_pdf(x)
         return round(self.pdf, round_to)
     
     def calculate_cdf(self, x, round_to=2):
-        """
+        """  Probability density function calculator for the Poisson distribution.
+            Args:
+                x (float): point for calculating the probability density function
+                round_to (int): Round the mean value. [Default value: 2 floating point]
+
+            Returns:
+                float: probability density function output
         """
         value = 0
         for i in range(x):
@@ -44,14 +79,26 @@ class Poisson(Distribution):
         return round(value, round_to)
     
     def _calc_discrete_pdf(x):
-        """
+        """ Internal function to calculate probability density function at a point.
+        Should not be used by end user.
+        
+        Args:
+            x (int): point for calculating the mean value.
         """
         fact = math.factorial(x)
         pdf = ( math.exp(-self.lmbda) * self.lmbda ** x) / fact
-        return round(pdf, round_to)
+        return pdf
     
     def plot_bar_pdf(self, points=100):
-        """
+        """ Method to plot the pdf of the Poisson distribution.
+
+        Args:
+            points (int): number of discrete data points
+
+        Returns:
+            list: x values for the pdf plot
+            list: y values for the pdf plot
+
         """
 
         x = []
@@ -69,12 +116,16 @@ class Poisson(Distribution):
         plt.xlabel("x")
 
         plt.show()
-        
+
         return x, y
 
     
     def __repr__(self):
-        """
+        """  Method to output the characteristics of the Poisson instace.
+        Args:
+            None
+        Returns:
+            string: characteristics of the Poisson
         """
 
         return "mean {0}, standard deviation {1}, lambda {2}".format(self.mean, self.stdev, self.lmbda)
